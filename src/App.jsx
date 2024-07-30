6f68903d4ae8a73d569c18260ffdbeefb4300534
rayan <rayanyasir993@gmail.com>
2024-07-30 10:22:51 +0500

    Front-End

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './sections/Header'
+import Sidebar from './sections/Sidebar'
+import Home from './sections/Home'
+
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
+  )
+}
+
+export default App
